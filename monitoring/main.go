83e9e537886fe9a93d4c2a6c3aa7ee38a740b272
zhuk <zhuk@>
2024-07-03 01:29:50 +0300

    add

---
+++ monitoring/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+	"time"
+)
+
+type metrics struct {
+}
+
+//func NewMetrics(reg prometheus.Registerer) *metrics {
+//	m := &metrics{
+//		devices: prometheus.NewGauge(prometheus.GaugeOpts{
+//			Namespace: "myapp",
+//			Name:      "connected_devices",
+//			Help:      "Number of currently connected devices.",
+//		}),
+//	}
+//	reg.MustRegister(m.devices)
+//	return m
+//}
+
+func main() {
+
+	usersOnline := prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "users_online",
+		})
+
+	go func() {
+		for {
+			for i := 0; i < 10000; i++ {
+				usersOnline.Set(float64(i)) // or: Inc(), Dec(), Add(5), Dec(5)
+				fmt.Println(i)
+				time.Sleep(10 * time.Millisecond)
+			}
+		}
+	}()
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":8080", nil)
+}
